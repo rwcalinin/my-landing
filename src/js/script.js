b44695e0e93e17fd0765ab59561559ef823658fd
rwcalinin <rwcalinin@gmail.com>
2021-04-28 13:14:00 +0300

    28apr update

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,21 @@ var typed = new Typed('#typing', {
    typeSpeed: 75,
    startDelay: 550
  });
+
+ var typed = new Typed('#about-typing', {
+  strings: ["a^500bout"],
+  typeSpeed: 75,
+  startDelay: 550
+});
+
+var typed = new Typed('#portfolio-typing', {
+  strings: ["my^500 por^200tfolio"],
+  typeSpeed: 75,
+  startDelay: 550
+});
+
+var typed = new Typed('#contact-typing', {
+  strings: ["c^300ontact^300 me"],
+  typeSpeed: 75,
+  startDelay: 550
+});
